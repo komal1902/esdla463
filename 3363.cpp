e0602831095d709cc099ee7a37a7e1d659c651c3
komal1902 <komal.sharma@cumminscollege.in>
2016-08-11 09:02:20 +0530

    Update 3363.cpp

--- 3363.cpp
+++ 3363.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int main(){
-  std::cout<<"Hello world";
-  std::cout<<"This is ESDL Lab";
+  std::cout<<"Hello world\n";
+  std::cout<<"This is ESDL Lab\n";
+  char name[50];
+  std::cin>>name;
+  std::cout<<"Hello "<<name<<"\n";
   return 0;
 }
